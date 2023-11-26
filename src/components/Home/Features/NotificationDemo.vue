e013676e7c6da32bf6026c607aa8319963cd5161
Anatolii Halas <tolikgalas@gmail.com>
2023-11-27 00:43:35 +0200

    Task 1. Enhanced @Features/Notifications at Home page

--- src/components/Home/Features/NotificationDemo.vue
+++ src/components/Home/Features/NotificationDemo.vue
@@ -1,19 +1,47 @@
 <script setup>
 import { ref } from "vue";
 
-const notifications = ref(null);
+const displayNotifications = ref(null);
+
+// Mock notifications
+const notifications = [
+  {
+    message: "You have been added to project.",
+    type: "info",
+  },
+  {
+    message: "has sent you a message",
+    type: "info",
+    sender: {
+      username: "Anakin",
+      image: "src/assets/img/anakin.png",
+    },
+  },
+];
+
+// Mock demo notifications
+const demoNotifications = [
+  {
+    message: "You have successfully registered",
+    type: "success",
+  },
+  {
+    message: "has sent you a message",
+    type: "info",
+    sender: {
+      username: "Obi Wan",
+      image: "src/assets/img/obi_wan.jpg",
+    },
+  },
+];
 
 const employee = {
   name: "Hart Hagerty",
-  employment: "Developer",
   image: "src/assets/img/user1.png",
-  job: {
-    vacancy: "Tech Lead Developer",
-  },
 };
 
 const showNotifications = () => {
-  let classList = notifications.value.classList;
+  let classList = displayNotifications.value.classList;
   classList.remove("hidden");
   setTimeout(() => {
     classList.add("hidden");
@@ -31,42 +59,52 @@ const showNotifications = () => {
       </button>
     </div>
     <div class="flex flex-col justify-between gap-4">
-      <div class="alert alert-success text-sm">
-        <span>You have successfully registered</span>
-      </div>
-      <div class="alert alert-info">
-        <span class="flex justify-start">
+      <div
+        v-for="(notification, index) in demoNotifications"
+        :key="index"
+        class="alert"
+        :class="'alert-' + notification.type"
+      >
+        <span v-if="notification.sender">
           <div class="flex items-center gap-3">
             <div class="avatar">
               <div class="mask mask-squircle h-12 w-12">
-                <img :src="employee.image" alt="Sender's avatar" />
+                <img :src="notification.sender.image" alt="Sender's avatar" />
               </div>
             </div>
             <div class="text-left">
-              <div class="font-bold">{{ employee.name }}</div>
-              <div class="text-sm opacity-50">has sent you message</div>
+              <div class="font-bold">{{ notification.sender.username }}</div>
+              <div class="text-sm opacity-50">{{ notification.message }}</div>
             </div>
           </div>
         </span>
+        <span v-else>
+          {{ notification.message }}
+        </span>
       </div>
     </div>
-    <div class="toast toast-end hidden" ref="notifications">
-      <div class="alert alert-info text-sm">
-        <span>You have been added to project</span>
-      </div>
-      <div class="alert alert-info">
-        <span class="flex justify-start">
+    <div class="toast toast-end hidden" ref="displayNotifications">
+      <div
+        v-for="(notification, index) in notifications"
+        :key="index"
+        class="alert"
+        :class="'alert-' + notification.type"
+      >
+        <span v-if="notification.sender">
           <div class="flex items-center gap-3">
             <div class="avatar">
               <div class="mask mask-squircle h-12 w-12">
-                <img :src="employee.image" alt="Sender's avatar" />
+                <img :src="notification.sender.image" alt="Sender's avatar" />
               </div>
             </div>
             <div class="text-left">
-              <div class="font-bold">{{ employee.name }}</div>
-              <div class="text-sm opacity-50">has sent you message</div>
+              <div class="font-bold">{{ notification.sender.username }}</div>
+              <div class="text-sm opacity-50">{{ notification.message }}</div>
             </div>
           </div>
+        </span>
+        <span v-else>
+          {{ notification.message }}
         </span>
       </div>
     </div>
